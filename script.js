a644e7d540d05657f51404adf22c4490c2a0a4e6
Gael Mukendi Kabongo <111666680+gmka98@users.noreply.github.com>
2022-10-30 17:01:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function NumberGuess() {
+  var guess = Math.floor(Math.random()*101)
+   console.log("Guess the number")
+   if(guess > ){
+    document.write("Too low");
+   }
+   if(guess < ){
+    document.write("too High")
+   }
+}
