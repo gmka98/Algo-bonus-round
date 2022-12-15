25a96319ea3fd53ce946aee2370d108b6c9b6b73
Gael Mukendi Kabongo <111666680+gmka98@users.noreply.github.com>
2022-12-15 16:22:53 +0100

    Create script.js

---
+++ 1 Number Guesser/script.js
@@ -0,0 +1,19 @@
+var guess = Math.floor(Math.random()*101)+1
+    console.log(guess);
+
+var number = 
+number > 101;
+function RandomNum(){
+    if (guess < number ){
+     alert("Too low")
+    }
+    
+    else if (guess > number){
+        alert('Too high')
+    }
+
+    else{
+        alert('Well guessed!')
+    }
+}
+Footer
