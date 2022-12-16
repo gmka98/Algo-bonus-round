759f1e3152e0b69fbf5288313c8033f2f21b5671
Gael Mukendi Kabongo <111666680+gmka98@users.noreply.github.com>
2022-12-16 11:11:27 +0100

    Update script.js

--- 3 Divisors/script.js
+++ 3 Divisors/script.js
@@ -1,1 +1,12 @@
-
+// function to print the divisors 
+function printDivisors(n) 
+{ 
+    for (i=1;i<=n;i++) 
+        if (n%i==0) 
+            document.write(i+ " "); 
+} 
+  
+/* Driver program to test above function */
+  
+    document.write("The divisors of 100 are:" + "<br>"); 
+    printDivisors(100);
