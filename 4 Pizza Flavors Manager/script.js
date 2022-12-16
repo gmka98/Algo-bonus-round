1d3b2aea7f112fc313fe69699b12bd42c8e80296
Gael Mukendi Kabongo <111666680+gmka98@users.noreply.github.com>
2022-12-16 17:40:01 +0100

    Update script.js

--- 4 Pizza Flavors Manager/script.js
+++ 4 Pizza Flavors Manager/script.js
@@ -1,1 +1,37 @@
+const body = document.querySelector('body')
+
+const h1 = document.createElement("h1");
+document.body.appendChild(h1);
+const textNode = document.createTextNode("Favorite Pizza Manager");
+h1.appendChild(textNode);
+
+
+const h2 = document.createElement("h2")
+document.body.appendChild(h2)
+const text2 = document.createTextNode('Hello! Welcome to the Pizza Flavors Manager.')
+h2.appendChild(text2)
+
+const ol = document.createElement('ol')
+document.body.appendChild(ol)
+
+const list = document.createElement('li')
+document.ol.appendChild(list)
+
+const li1Node = document.createTextNode("List all the pizza flavors")
+document.li1.appendChild(li1Node)
+
+const li2 = document.createElement('li')
+const li2Node = document.createTextNode("Add a new pizza flavor")
+document.ol.appendChild(li2)
+document.li2.appendChild(li2Node)
+
+const li3 = document.createElement('li')
+const li3Node = document.createTextNode("Remove a new pizza flavor")
+document.ol.appendChild(li1)
+document.li3.appendChild(li3Node)
+
+const li4 = document.createElement('li')
+const li4Node = document.createTextNode("Exit this program")
+document.ol.appendChild(li4)
+document.li4.appendChild(li4Node)
 
