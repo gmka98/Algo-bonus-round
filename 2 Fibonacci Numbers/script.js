cdf5fd0b198272bb7620ff8a7b857b2e7e6d6a06
Gael Mukendi Kabongo <111666680+gmka98@users.noreply.github.com>
2022-12-16 03:10:14 +0100

    Update script.js

--- 2 Fibonacci Numbers/script.js
+++ 2 Fibonacci Numbers/script.js
@@ -1,1 +1,23 @@
+// Function to print
+// first n Fibonacci Numbers
+function printFibonacciNumbers(n)
+{
+	let f1 = 0
+    let f2 = 1
+     i;
+
+	if (n < 1)
+		return;
+	document.write(f1 + " ");
+	for (i = 1; i < n; i++) {
+		console.log(f2 + " ");
+		let next = f1 + f2;
+		f1 = f2;
+		f2 = next;
+	}
+}
+
+	printFibonacciNumbers(100);
+	
+
 
